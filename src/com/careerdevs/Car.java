63cd52b99f9e91e13da73b0bb38dd55554d12036
marcusj12 <marcus.a.jennings12@gmail.com>
2022-03-22 18:22:35 -0400

    CarRentalV3

--- src/com/careerdevs/Car.java
+++ src/com/careerdevs/Car.java
@@ -15,11 +15,12 @@ public class Car {
     initialize - is the process of locating and using the defined values for variable data that is used by a computer program.
     Important role in retrieving and updating the value of a variable outside the "encapsulating" class.
     Encapsulating - the idea of bundling data and methods that work on that data within one unit;the purpose is to hide
-    sensitive data from users in order to access and update the private variable.
+    sensitive data from users in order to access and update the private variable. (4 pillars of OOP).
     Setter updates the value of a variable
     Getters read the value of a variable
-    **Accessing variables outside a class wothout getters/setters, we have to mark those as public, protected, or default.
-    Essentially losing
+    **Accessing variables outside a class without getters/setters, we have to mark those as public, protected, or default.
+    Essentially losing control over data---- thus compromising fundamental OOp principle encapsulation.--- since anyone can
+    can change the non-private fields outside the class directly... thus we can not achieve immutability**
      */
 
     public String getMake() {
