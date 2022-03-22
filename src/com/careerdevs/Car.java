001a59f9c779bc5ca8b42cc228880f6eb50e357f
marcusj12 <marcus.a.jennings12@gmail.com>
2022-03-22 18:15:14 -0400

    Car Rental V3

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,49 @@
+package com.careerdevs;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+// Car Class Constructor
+    public Car(String make,String model,int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    /*
+    Constructors are used to initialize objects; "Classes represent real world objects & objects are made within classes"
+    initialize - is the process of locating and using the defined values for variable data that is used by a computer program.
+    Important role in retrieving and updating the value of a variable outside the "encapsulating" class.
+    Encapsulating - the idea of bundling data and methods that work on that data within one unit;the purpose is to hide
+    sensitive data from users in order to access and update the private variable.
+    Setter updates the value of a variable
+    Getters read the value of a variable
+    **Accessing variables outside a class wothout getters/setters, we have to mark those as public, protected, or default.
+    Essentially losing
+     */
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
