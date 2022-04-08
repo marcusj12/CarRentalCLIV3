15f573a17c10fd4f75be7a9502493cfcd5fbfd12
marcusj12 <marcus.a.jennings12@gmail.com>
2022-04-08 10:55:18 -0400

    CarRentalCLIV3- part 2

--- src/com/careerdevs/Dealership.java
+++ src/com/careerdevs/Dealership.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Dealership {
     private String dealershipName;
-    private List<Car>availableCars;
-    private List<Car>soldCars;
+    private List<Car> availableCars;
+    private List<Car> soldCars;
 
     public Dealership(String dealershipName) {
         this.dealershipName = dealershipName;
@@ -15,51 +15,53 @@ public class Dealership {
 // Properties we're giving to teh class; we're blueprinting how we want things to return. Everytime we run the program it
     }
 
-    public String getDealershipName () {
+    public String getDealershipName() {
         return dealershipName;
     }
 
-    public void setDealershipName (String dealershipName) {
+    public void setDealershipName(String dealershipName) {
         this.dealershipName = dealershipName;
     }
 
-    public List<Car> getAvailableCars(){
+    public List<Car> getAvailableCars() {
         return availableCars;
     }
 
-    public void setAvailableCars(List<Car> availableCars){
+    public void setAvailableCars(List<Car> availableCars) {
         this.availableCars = availableCars;
     }
 
-    public List<Car> getSoldCars(){
+    public List<Car> getSoldCars() {
         return soldCars;
     }
 
-    public void setSoldCars(List<Car>soldCars) {
+    public void setSoldCars(List<Car> soldCars) {
         this.soldCars = soldCars;
     }
-/*
-What is the Override/toString method? What is its purpose and functionality?
-- crafting a string that is readable to users. You're manipulating how the pointer displays the data to users.
- */
+
+    /*
+    What is the Override/toString method? What is its purpose and functionality?
+    - crafting a string that is readable to users. You're manipulating how the pointer displays the data to users.
+     */
     @Override
-    public String toString(){
+    public String toString() {
         return "dealershipName='" + dealershipName + '\'' +
                 ", availableCars='" + availableCars + '\'' +
                 ", soldCars='" + soldCars;
     }
-/*
-The loop below is programmatically allowing users to select a car. Upon selection the car is added to the sold car array &
-    removed from the available cars - demon
 
- */
+    /*
+    The loop below is programmatically allowing users to select a car. Upon selection the car is added to the sold car array &
+        removed from the available cars - demon
+     the toString method allows imput and data to be seen by users. We do this because data that will display would be the location in memory.
+     */
     //todo How to fly?
     public void buyCar(String license) {
         if (availableCars.size() == 0) {
             System.out.println("Sorry there is no cars available");
             return;
         }
-        for (int i =0; i < availableCars.size(); i++) {
+        for (int i = 0; i < availableCars.size(); i++) {
             if (license.equals(availableCars.get(i).getLicensePlate())) {
                 //System.out.println("You have selected a " + availableCars.get(i) + "for  a rental");
                 soldCars.add(availableCars.get(i));
@@ -69,6 +71,7 @@ The loop below is programmatically allowing users to select a car. Upon selectio
         }
         System.out.println("The vehicle you've selected is not available, please select from our inventory");
     }
+
     /* The loop below is the inverse of the loop above. The loop mimics a user returning a vehicle and it being added back
     to the available arrays
    */
@@ -87,18 +90,13 @@ The loop below is programmatically allowing users to select a car. Upon selectio
         }
         System.out.println("The vehicle you've selected has not sold yet");
     }
+
     /*
      */
     public void displayInventory() {
-        for (int i =0; i <availableCars.size(); i++){
+        for (int i = 0; i < availableCars.size(); i++) {
             System.out.println(availableCars.get(i));
         }
-
-    }
-
-    public void mainMenu (){
-        System.out.println("1) Buy Car");
-
     }
 }
 
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-       System.out.println("What would you like to do?");
-       String userInput = scan.nextLine();
-       System.out.println("What car would you like to rent, based off of license plate?");
+          Scanner scanner = new Scanner(System.in);
+//       System.out.println("What would you like to do?");
+       //String userInput = scanner.nextLine();
+//       System.out.println("What car would you like to rent, based off of license plate?");
 
 
        Car rental1 = new Car("Honda", "Accord", "TW564",2018);
@@ -25,21 +25,67 @@ public class Main {
        dealership1.getAvailableCars().add(rental4);
        dealership1.getAvailableCars().add(rental5);
 
+        System.out.println("Hello! Welcome to the " + dealership1.getDealershipName() + "dealership ") ;
+
+        while (true){
+            System.out.println("Would you like to do: (1) Buy a Car (2) Return a Car (3) Leave the dealership");
+            int res = 0;
+
+            try{ // we have code, taht is prone to throw an exception;
+                res = scanner.nextInt();
+                scanner.nextLine();
+            } catch (Exception ex) {
+
+            }
+
+            if (res == 3) {
+                System.out.println("You are now leave the dealership");
+                break;
+            } else if (res ==2){
+                //return logic here
+                System.out.println("Which car would you like to return? Input license plate");
+                for (int i =0; i <dealership1.getSoldCars().size(); i++) {
+                    System.out.println(dealership1.getSoldCars().get(i));
+                }
+
+                String userInput = scanner.next();
+                dealership1.returnCar(userInput);
+                System.out.println(dealership1.getAvailableCars());
+                System.out.println(dealership1.getSoldCars());
+
+            }else if (res == 1){
+                //buy car logic here
+                System.out.println("Which car would you like to rent? Input license plate");
+                for (int i =0; i <dealership1.getAvailableCars().size(); i++) {
+                    System.out.println(dealership1.getAvailableCars().get(i));
+            }
+                String userInput = scanner.next();
+                dealership1.buyCar(userInput);
+                System.out.println(dealership1.getAvailableCars());
+                System.out.println(dealership1.getSoldCars());
+
+            } else {
+                System.out.println("");
+            }
+        }
         //System.out.println(dealership1.getAvailableCars());
 
 //        for (int i =0; i < dealership1.getAvailableCars().size();i++){
 //            System.out.println(dealership1.getAvailableCars().get(i));
 //        }
         dealership1.displayInventory();
-        String userInput1 = scan.nextLine();
+        String userInput1 = scanner.nextLine();
         dealership1.buyCar(userInput1);
 
-
         System.out.println("Thank you for renting with" + " "+  dealership1.getDealershipName() + "," + " " + "we hope you enjoy your rental experience in our "+ dealership1.getSoldCars());
-
        }
-
     }
 
 
-
+/*
+While loops run indefinitely we have to find a way to break out of the loop
+In a business sense, we want to make sure the logic of the program and our menu is sound. The issue we were having or
+rather the issue we are trying to remedy is the break in the code. If a user inputs a response that doesn't match
+the data It's trying to access then it defaults to the main menu. If users have already indicated that they already wanted
+to buy or rent a vehicle, why should they be asked again.
+ */
